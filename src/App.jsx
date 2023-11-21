d57d75d2b74e003d2ea21b80fc7c915ef16a5c98
Carlos Gomes <carlos_gomes1809@hotmail.com>
2023-11-21 11:34:29 -0300

    set up Vite with React & TailwindCSS as well as models

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <main className="bg-slate-300/20 relative min-h-screen">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </main>
+  );
+}
+
+export default App;
